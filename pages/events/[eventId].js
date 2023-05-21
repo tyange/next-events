f00bc2ae26a71b6b49d54c33152b27f3817f5954
tyange <usun16@gmail.com>
2023-05-21 11:58:47 +0900

    get event detail from dummy-backend

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,33 +1,55 @@
 import { Fragment } from "react";
-import { useRouter } from "next/router";
 
-import { getEventById } from '../../dummy-data'
+import { getEventById, getAllEvents } from "../../helpers/api-util";
 
-import EventSummary from "../../components/event-detail/event-summary"
-import EventLogistics from '../../components/event-detail/event-logistics'
-import EventContent from "../../components/event-detail/event-content"
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
 
-
-function EventDetailPage() {
-  const router = useRouter();
-
-  const eventId = router.query.eventId;
-
-  const event = getEventById(eventId);
+function EventDetailPage(props) {
+  const event = props.selectedEvent;
 
   if (!event) {
-    return <p>No Event found!</p>
+    return <p>No Event found!</p>;
   }
 
   return (
     <Fragment>
       <EventSummary title={event.title} />
-      <EventLogistics date={event.date} address={event.location} image={event.image} imageAlt={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
       <EventContent>
         <p>{event.description}</p>
       </EventContent>
     </Fragment>
   );
+}
+
+export async function getStaticProps(context) {
+  const eventId = context.params.eventId;
+
+  const event = await getEventById(eventId);
+
+  return {
+    props: {
+      selectedEvent: event,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const events = await getAllEvents();
+
+  const paths = events.map((event) => ({ params: { eventId: event.id } }));
+
+  return {
+    paths: paths,
+    fallback: false,
+  };
 }
 
 export default EventDetailPage;
